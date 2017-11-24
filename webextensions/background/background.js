ff27b95288fe58c4360c71bc39e579e0502a12e1
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-24 15:45:21 +0900

    Reopen as child tab only if it is same origin

--- webextensions/background/background.js
+++ webextensions/background/background.js
@@ -5,9 +5,50 @@
 */
 'use strict';
 
+var gTabs = {};
+
+browser.tabs.query({}).then(aTabs => {
+  for (let tab of aTabs) {
+    gTabs[tab.id] = {
+      url: tab.url
+    };
+  }
+});
+
+browser.tabs.onCreated.addListener(aTab => {
+  gTabs[aTab.id] = {
+    url: aTab.url
+  };
+});
+
+browser.tabs.onUpdated.addListener((aTabId, aChangeInfo, aTab) => {
+  if (!('url' in aChangeInfo))
+    return;
+  let tab = gTabs[aTab.id];
+  tab.previousUrl = tab.url;
+  tab.url = aChangeInfo.url;
+});
+
+browser.tabs.onRemoved.addListener(aTabId => {
+  delete gTabs[aTabId];
+});
+
+browser.windows.onRemoved.addListener(aWindowId => {
+  for (let tabId of Object.keys(gTabs)) {
+    if (gTabs[tabId].windowId == aWindowId)
+      delete gTabs[tabId];
+  }
+});
+
 browser.webNavigation.onCommitted.addListener(
   aDetails => {
     if (aDetails.transitionType != 'typed')
+      return;
+
+    var tab = gTabs[aDetails.tabId];
+    var url = tab.previousUrl || tab.url;
+    if (url == 'about:blank' ||
+        /^about:(newtab|home)$/.test(url))
       return;
 
     browser.tabs.executeScript(aDetails.tabId, {
@@ -18,10 +59,14 @@ browser.webNavigation.onCommitted.addListener(
         active: true,
         url:    aDetails.url
       };
-      var isSameDomain = true; // TBD
-      if (isSameDomain)
+      if (extractOriginPart(url) == extractOriginPart(aDetails.url))
         params.openerTabId = aDetails.tabId;
       browser.tabs.create(params);
     });
   }
 );
+
+function extractOriginPart(aURI) {
+  var uriMatch = aURI.match(/^(\w+:\/\/[^\/]+)/);
+  return uriMatch && uriMatch[1];
+}
